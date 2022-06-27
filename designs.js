d23a0fdc5dd3ba2483210687ff6aba59d67a988e
Phillip Gooden <phillipgooden@gmail.com>
2022-06-26 22:41:51 -0500

    style: improve readability of code

--- designs.js
+++ designs.js
@@ -1,61 +1,60 @@
 // submit input const
 const submitGrid = document.querySelector("input[type=submit]");
-const COLOR = document.getElementById("colorPicker")
-let changeColor = undefined;
-COLOR.addEventListener("input", function onClick(el){
-  changeColor = el.target.value;
+const COLOR = document.getElementById("colorPicker");
+let changeColor;
+COLOR.addEventListener("input", function onClick(el) {
+	changeColor = el.target.value;
 });
 
 // Select size input
-const HEIGHT = document.getElementById("inputHeight") //store height in const
+const HEIGHT = document.getElementById("inputHeight"); //store height in const
 let newHeight = 1; //set height initial value
-HEIGHT.addEventListener("input", function(el){
-  newHeight = el.target.value;
-});//update the columns
+HEIGHT.addEventListener("input", function(el) {
+	newHeight = el.target.value;
+}); //update the columns
 
 
-const WIDTH = document.getElementById("inputWidth") //store width in const
+const WIDTH = document.getElementById("inputWidth"); //store width in const
 let newWidth = 1; //set width initial value
-WIDTH.addEventListener("input", function(el){
-  newWidth = el.target.value;
-});//update rows
+WIDTH.addEventListener("input", function(el) {
+	newWidth = el.target.value;
+}); //update rows
 
 
 // When size is submitted by the user, call makeGrid()
 function makeGrid() {
-  const body = document.getElementsByTagName("body")
-  const table = document.getElementById("pixelCanvas")
-  const tableBody = document.createElement("tbody")
+	// const body = document.getElementsByTagName("body");
+	const table = document.getElementById("pixelCanvas");
+	const tableBody = document.createElement("tbody");
 
-  if(table.firstChild) {
-    table.firstChild.remove();
-  }
+	if (table.firstChild) {
+		table.firstChild.remove();
+	}
 
-  for( let r = 0; r < newWidth; r++){
-    const row = document.createElement("tr");
-  
-  for( let c = 0; c < newHeight; c++){
-    const column = document.createElement("td");
-    row.appendChild(column);
-  };
-  tableBody.appendChild(row);
-};
-  table.appendChild(tableBody);
-  
+	for (let r = 0; r < newWidth; r++) {
+		const row = document.createElement("tr");
 
-  const td = document.getElementsByTagName("td");
-  for(let i = 0; i < td.length; i++) {
-    td[i].onclick = function() {
-      this.style.backgroundColor = changeColor
-    }
-    console.log("hello");
-    }
+		for (let c = 0; c < newHeight; c++) {
+			const column = document.createElement("td");
+			row.appendChild(column);
+		}
+		tableBody.appendChild(row);
+	}
+	table.appendChild(tableBody);
+
+
+	const td = document.getElementsByTagName("td");
+	for (let i = 0; i < td.length; i++) {
+		td[i].onclick = function() {
+			this.style.backgroundColor = changeColor;
+		};
+	}
 }
 
 
 const form = document.getElementById("sizePicker");
 form.addEventListener("submit", function(el) {
-  el.preventDefault();
+	el.preventDefault();
 });
 
 submitGrid.addEventListener("click", makeGrid, false);
